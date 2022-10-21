ef4a6b520a5877fb4d65f46686e767b17b922b72
Diene89 <diene_89@hotmail.com>
2022-10-21 19:18:47 -0300

    implementa teste da função sort_by que espera que a pessoa usuária possa escolher um critério de ordenação para exibir os empregos

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,25 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+JOBS_SALARY = [
+    {
+        "min_salary": 25000,
+        "max_salary": 50000,
+        "date_posted": "2022-10-21",
+    },
+    {
+        "min_salary": 5000,
+        "max_salary": 10000,
+        "date_posted": "2022-10-20",
+    },
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(JOBS_SALARY, "min_salary")
+    assert JOBS_SALARY[0]["min_salary"] == 5000
+
+    sort_by(JOBS_SALARY, "max_salary")
+    assert JOBS_SALARY[0]["max_salary"] == 50000
+
+    sort_by(JOBS_SALARY, "date_posted")
+    assert JOBS_SALARY[0]["date_posted"] == "2022-10-21"
