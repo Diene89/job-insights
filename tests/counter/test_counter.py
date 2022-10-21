7fcd526820321ccb23d378e87a2208533573006d
Diene89 <diene_89@hotmail.com>
2022-10-21 16:43:36 -0300

    test: implementa teste da função counter_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.counter import count_ocurrences
+import json
+from unittest.mock import mock_open, patch
+
+from src.counter import count_ocurrences
+
+WORDS = {"result": "A Trybe é uma escola de desenvolvimento Web"}
 
 
 def test_counter():
-    pass
+    with patch("builtins.open", mock_open(read_data=json.dumps(WORDS))):
+        assert count_ocurrences("any", "Trybe") == 1
